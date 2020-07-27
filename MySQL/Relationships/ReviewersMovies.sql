38638c4e9aa7ed037d1569085bebb42294d30c68
Jacob Janowski <jjanow7@uic.edu>
2020-07-27 16:02:12 +0000

    many to many relationships complete.

--- MySQL/Relationships/ReviewersMovies.sql
+++ MySQL/Relationships/ReviewersMovies.sql
@@ -101,4 +101,49 @@ FROM series
 LEFT JOIN reviews
     ON series.id = reviews.series_id
 WHERE rating IS NULL;
+
+
+# Exercise 5
+SELECT genre, 
+       Round(Avg(rating), 2) AS avg_rating 
+FROM   series 
+       INNER JOIN reviews 
+               ON series.id = reviews.series_id 
+GROUP  BY genre;
+
+
+# Exercise 6
+SELECT first_name, 
+       last_name, 
+       Count(rating)                    AS COUNT, 
+       Ifnull(Min(rating), 0)           AS MIN, 
+       Ifnull(Max(rating), 0)           AS MAX, 
+       Round(Ifnull(Avg(rating), 0), 2) AS AVG, 
+       CASE 
+         WHEN Count(rating) >= 10 THEN 'POWER USER' 
+         WHEN Count(rating) > 0 THEN 'ACTIVE' 
+         ELSE 'INACTIVE' 
+       end                              AS STATUS 
+FROM   reviewers 
+       LEFT JOIN reviews 
+              ON reviewers.id = reviews.reviewer_id 
+GROUP  BY reviewers.id; 
+
+
+# Exercise 7
+
+SELECT 
+    title,
+    rating,
+    CONCAT(first_name,' ', last_name) AS reviewer
+FROM reviewers
+INNER JOIN reviews 
+    ON reviewers.id = reviews.reviewer_id
+INNER JOIN series
+    ON series.id = reviews.series_id
+ORDER BY title;
+
+
+
+
     
