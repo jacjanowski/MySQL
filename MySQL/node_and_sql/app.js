6b458b472da8ec24a8c7e65459f491b940294da6
Jacob Janowski <jjanow7@uic.edu>
2020-08-05 14:55:16 +0000

    schema and query connections.

--- MySQL/node_and_sql/app.js
+++ MySQL/node_and_sql/app.js
@@ -1,7 +1,22 @@
 var mysql = require('mysql');
+var faker = require('faker');
 
+ 
 var connection = mysql.createConnection({
-	host: "localhost",
-	user: "",
-	database: "JoinUs"
+  host     : 'localhost',
+  user     : 'root',
+  // password: process.ENV.MYSQL_PW,
+  database : 'join_us'
 });
+
+var person = {
+    email: faker.internet.email(),
+    created_at: faker.date.past()
+};
+ 
+var end_result = connection.query('INSERT INTO users SET ?', person, function(err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+connection.end();
